a40594e2c0700e058e0b20e978f2a1c39f4f4f4e
Ray Alcides <railayli2@hotmail.com>
2023-02-27 18:43:25 -0300

    styles do menu e do banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,18 +1,20 @@
 import perfil from '../assets/img/perfil.png'
 
+import styles from '../styles/components/banner.module.css'
+
 export function Banner() {
   return (
-    <div>
-     <div>
+    <div className={styles.bannerContainer}>
+     <div className={styles.bannerInformacoes}>
        <p>
          <strong>OLÁ, sejá bem vindo(a), me chamo</strong>
        </p>
        <h1>RAIANI ALCIDES</h1>
        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum architecto sint, rerum labore provident, dolorum, praesentium eius natus quasi quae pariatur esse aspernatur tempora laborum beatae ea recusandae minus reiciendis.</p>
-       <button>Meus jobs</button>
+       <button className={styles.bannerClick}>Meus jobs</button>
      </div>
      <div>
-      <img src={perfil} alt="" />
+      <img className={styles.bannerImg} src={perfil} alt="" />
      </div>
     </div>
   )
